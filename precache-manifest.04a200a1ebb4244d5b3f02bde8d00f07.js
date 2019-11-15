9a11d273efae0035fe945047de2dd07e0e3e9590
Roberto Samia <bojosteph@gmail.com>
2019-11-14 21:39:50 -0500

    Updates

---
+++ precache-manifest.04a200a1ebb4244d5b3f02bde8d00f07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e23b80ed37eb57b96479f10bbfc633",
+    "url": "/react-robofriends/index.html"
+  },
+  {
+    "revision": "8564b567b2e50ee300ca",
+    "url": "/react-robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b92a80f325be41f5725a",
+    "url": "/react-robofriends/static/css/main.e97f1c14.chunk.css"
+  },
+  {
+    "revision": "8564b567b2e50ee300ca",
+    "url": "/react-robofriends/static/js/2.08bf0e13.chunk.js"
+  },
+  {
+    "revision": "b92a80f325be41f5725a",
+    "url": "/react-robofriends/static/js/main.22351ea4.chunk.js"
+  },
+  {
+    "revision": "8f2d44ca28f742a96e01",
+    "url": "/react-robofriends/static/js/runtime-main.33eb9e57.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/react-robofriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
