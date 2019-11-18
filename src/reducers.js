f7684fe7da2ff1733957efc12af1e28900365962
Roberto Samia <bojosteph@gmail.com>
2019-11-17 21:52:22 -0500

    added redux to app for state management

---
+++ src/reducers.js
@@ -0,0 +1,15 @@
+import { CHANGE_SEARCH_FIELD } from "./constants";
+
+const initialStateSearch = {
+  searchField: ""
+};
+
+export const searchRobots = (state = initialStateSearch, action = {}) => {
+  // console.log(action.type);
+  switch (action.type) {
+    case CHANGE_SEARCH_FIELD:
+      return Object.assign({}, state, { searchField: action.payload });
+    default:
+      return state;
+  }
+};
